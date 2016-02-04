b1aa3a7dc6ae129651d9c86b06267a872bd87f92
mohrtw <mohrtw@gmail.com>
2016-02-03 19:27:35 -0800

    refactored tests.py to allow the same tests to be ran on different sorting algorithms

---
+++ algorithms/tests/test_sorting.py
@@ -0,0 +1,74 @@
+import unittest
+from unittest import TestCase
+
+from algorithms.sorting.sorting import quickSort
+
+def SortingTest(sortFcn):
+    
+    class sortTest(TestCase):
+    
+        def test_empty_list(self):
+            xs = sortFcn([])
+            self.assertEqual(xs,[])
+    
+        def test_single_item(self):
+            xs = sortFcn([1])
+            self.assertEqual(xs,[1])
+            
+        def test_two_numbers(self):
+            xs = sortFcn([2,1])
+            self.assertEqual(xs, [1,2])
+            
+            xs = sortFcn([2,1])
+            self.assertEqual(xs, [1,2])
+            
+        def test_two_identical_numbers(self):
+            xs = sortFcn([2,2])
+            self.assertEqual(xs, [2,2])
+            
+            xs = sortFcn([0,0])
+            self.assertEqual(xs, [0,0])
+            
+            xs = sortFcn([-3,-3])
+            self.assertEqual(xs, [-3,-3])
+            
+        def test_two_numbers_one_negative(self):
+            xs = sortFcn([-2,1])
+            self.assertEqual(xs, [-2,1])
+            
+            xs = sortFcn([1,-2])
+            self.assertEqual(xs, [-2,1])
+            
+        def test_two_negative_numbers(self):
+            xs = sortFcn([-2,-4])
+            self.assertEqual(xs, [-4,-2])
+            
+            xs = sortFcn([-4,-2])
+            self.assertEqual(xs, [-4,-2])
+            
+        def test_three_numbers(self):
+            xs = sortFcn([0,1,2])
+            self.assertEqual(xs,[0,1,2])
+            
+            xs = sortFcn([0,2,1])
+            self.assertEqual(xs,[0,1,2])
+            
+            xs = sortFcn([1,0,2])
+            self.assertEqual(xs, [0,1,2])
+            
+            xs = sortFcn([1,2,0])
+            self.assertEqual(xs, [0,1,2])
+            
+            xs = sortFcn([2,0,1])
+            self.assertEqual(xs, [0,1,2])
+            
+            xs = sortFcn([2,1,0])
+            self.assertEqual(xs, [0,1,2])
+            
+    return sortTest
+    
+
+qsortTest = SortingTest(quickSort)
+
+if __name__ == '__main__':
+    unittest.main()
