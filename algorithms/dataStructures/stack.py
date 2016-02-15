44a857aa8473e2870588dbd45dda5175b9c1abda
mohrtw <mohrtw@gmail.com>
2016-02-14 17:25:33 -0800

    added stack data structure

---
+++ algorithms/dataStructures/stack.py
@@ -0,0 +1,11 @@
+class Stack(object):
+    
+    def __init__(self):
+        self.items = []
+        self.size = 0
+        
+    def push(self,item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
