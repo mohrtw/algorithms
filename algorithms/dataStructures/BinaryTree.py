b78134a2da7b793932dbeb026e407f7b9dbaa0b3
mohrtw <mohrtw@gmail.com>
2016-02-21 16:58:12 -0800

    added Binary Tree data structure

---
+++ algorithms/dataStructures/BinaryTree.py
@@ -0,0 +1,81 @@
+from algorithms.dataStructures.stack import Stack
+
+from algorithms.utils.exceptions import returnValue
+
+
+class BinaryTreeNode():
+
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+        if self.left:
+            self.left.parent = self
+        if self.right:
+            self.right.parent = self
+
+    def transverse_inorder(self, fcn=None, *args, **kwargs):
+        s = Stack()
+        s.push(self)
+
+        while not s.is_empty():
+            # if the top of the stack is a node, attempt to add its
+            # children to the stack,
+            # otherwise, call fcn on the top item of the stack
+            if isinstance(s.peek(), BinaryTreeNode):
+                curNode = s.pop()
+
+                if curNode.right:
+                    s.push(curNode.right)
+
+                if fcn:
+                    s.push((curNode,))
+
+                if curNode.left:
+                    s.push(curNode.left)
+
+            else:
+                curNode, = s.pop()
+                try:
+                    fcn(curNode.value, *args, **kwargs)
+                except returnValue as v:
+                    return v.value
+
+    def transverse_preorder(self, fcn=None, *args, **kwargs):
+        if fcn:
+            fcn(self.value, *args, **kwargs)
+        if self.left:
+            self.left.transverse_preorder(fcn, *args, **kwargs)
+        if self.right:
+            self.right.transverse_preorder(fcn, *args, **kwargs)
+
+    def transverse_postorder(self, fcn=None, *args, **kwargs):
+        if self.left:
+            self.left.transverse_postorder(fcn, *args, **kwargs)
+        if self.right:
+            self.right.transverse_postorder(fcn, *args, **kwargs)
+
+        if fcn:
+            fcn(self.value, *args, **kwargs)
+
+
+class BinaryTree():
+
+    def __init__(self, root=None):
+        self.root = root
+
+    def is_empty(self):
+        return self.root is None
+
+    def contains(self, value):
+
+        def raiseReturnTrueIfMatches(nodeValue, compValue):
+            if nodeValue == compValue:
+                raise returnValue(True)
+
+        if self.root.transverse_inorder(raiseReturnTrueIfMatches, value):
+            return True
+
+        return False
---
+++ algorithms/tests/test_binary_tree.py
@@ -0,0 +1,144 @@
+import unittest
+from unittest import TestCase
+
+from algorithms.dataStructures.BinaryTree import BinaryTreeNode
+from algorithms.dataStructures.BinaryTree import BinaryTree
+
+
+class binary_tree_creation_Test(TestCase):
+
+    def test_create_node(self):
+        n = BinaryTreeNode(1)
+        self.assertIsInstance(n, BinaryTreeNode)
+
+    def test_create_node_assigns_correct_data(self):
+        n = BinaryTreeNode(1)
+        self.assertEqual(1, n.value)
+
+    def test_create_node_assigns_None_to_left_right_parent(self):
+        n = BinaryTreeNode(1)
+        self.assertIsNone(n.left)
+        self.assertIsNone(n.right)
+        self.assertIsNone(n.parent)
+
+    def test_create_binary_tree(self):
+        bt = BinaryTree()
+        self.assertIsInstance(bt, BinaryTree)
+
+    def test_create_binary_tree_with_one_node(self):
+        n = BinaryTreeNode(1)
+        bt = BinaryTree(n)
+        self.assertEqual(n, bt.root)
+        self.assertEqual(1, bt.root.value)
+        self.assertIsNone(bt.root.parent)
+
+    def test_create_linked_list_with_multiple_nodes(self):
+        nr = BinaryTreeNode(3)
+        nl = BinaryTreeNode(2)
+        n = BinaryTreeNode(1, nl, nr)
+
+        bt = BinaryTree(n)
+        self.assertEqual(n, bt.root)
+        self.assertEqual(1, bt.root.value)
+
+        self.assertEqual(nl, bt.root.left)
+        self.assertEqual(2, bt.root.left.value)
+
+        self.assertEqual(nr, bt.root.right)
+        self.assertEqual(3, bt.root.right.value)
+
+
+class empty_Test(TestCase):
+
+    def test_empty_Tree(self):
+        bt = BinaryTree()
+        self.assertTrue(bt.is_empty())
+
+    def test_one_element_Tree(self):
+        bt = BinaryTree(BinaryTreeNode(1))
+        self.assertFalse(bt.is_empty())
+
+
+class transversal_Test(TestCase):
+
+    def setUp(self):
+        nrr = BinaryTreeNode(7)
+        nrl = BinaryTreeNode(6)
+        nlr = BinaryTreeNode(5)
+        nll = BinaryTreeNode(4)
+        nr = BinaryTreeNode(3, nrl, nrr)
+        nl = BinaryTreeNode(2, nll, nlr)
+        n = BinaryTreeNode(1, nl, nr)
+
+        self.bt = BinaryTree(n)
+
+    def test_inorder(self):
+        output = ''
+
+        def fcn(value):
+            nonlocal output
+            output += str(value) + ' '
+
+        self.bt.root.transverse_inorder(fcn)
+
+        self.assertEqual("4 2 5 1 6 3 7 ", output)
+
+    def test_preorder(self):
+        output = ''
+
+        def fcn(value):
+            nonlocal output
+            output += str(value) + ' '
+
+        self.bt.root.transverse_preorder(fcn)
+
+        self.assertEqual("1 2 4 5 3 6 7 ", output)
+
+    def test_postorder(self):
+        import sys
+        from io import StringIO
+
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            self.bt.root.transverse_postorder(print)
+            output = out.getvalue().strip()
+            self.assertEqual(output, "4\n5\n2\n6\n7\n3\n1")
+        finally:
+            sys.stdout = saved_stdout
+
+
+class contains_Test(TestCase):
+
+    def setUp(self):
+        nrr = BinaryTreeNode(7)
+        nrl = BinaryTreeNode(6)
+        nlr = BinaryTreeNode(5)
+        nll = BinaryTreeNode(4)
+        nr = BinaryTreeNode(3, nrl, nrr)
+        nl = BinaryTreeNode(2, nll, nlr)
+        n = BinaryTreeNode(1, nl, nr)
+
+        self.bt = BinaryTree(n)
+
+    def test_contains(self):
+        self.assertTrue(self.bt.contains(1))
+        self.assertTrue(self.bt.contains(2))
+        self.assertTrue(self.bt.contains(3))
+        self.assertTrue(self.bt.contains(4))
+        self.assertTrue(self.bt.contains(5))
+        self.assertTrue(self.bt.contains(6))
+        self.assertTrue(self.bt.contains(7))
+
+    def test_does_not_contain(self):
+        self.assertFalse(self.bt.contains(8))
+        self.assertFalse(self.bt.contains(0))
+        self.assertFalse(self.bt.contains('a'))
+        self.assertFalse(self.bt.contains(45))
+        self.assertFalse(self.bt.contains((1, 2)))
+        self.assertFalse(self.bt.contains("tree"))
+
+
+if __name__ == '__main__':
+    unittest.main()
